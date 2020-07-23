6320969e46e262a249ecf30286eafa4475f290b5
leticianb <leticianakahara10@gmail.com>
2020-07-23 00:34:20 -0300

    oioi

--- js/function-menu.js
+++ js/function-menu.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
-            //   $('#conteudo').load('src/conexao/disciplina/modelo/visao/listdisciplina.html');
-            //})
+    //   $('#conteudo').load('src/conexao/disciplina/modelo/visao/listdisciplina.html');
+    //})
+    $('#adiciona').click(function(e) {
+        e.preventDefault()
+        $('#conteudo').empty()
+        $('#conteudo').load('src/disciplinas/visao/adiciona-disciplinas.html')
+    })
+
+})
