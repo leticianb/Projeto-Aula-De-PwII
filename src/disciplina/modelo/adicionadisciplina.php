5c3bdd617d2d53070675cae627facdfd1daacd32
leticianb <leticianakahara10@gmail.com>
2020-08-25 11:36:31 -0300

    ii

--- src/disciplina/modelo/adicionadisciplina.php
+++ src/disciplina/modelo/adicionadisciplina.php
@@ -1,33 +1,33 @@
 <?php
  
  
- include('../../conexao/conn.php');
+include('../../conexao/conn.php');
  
- $nome = $_REQUEST['nome'];
- $professor = $_REQUEST['professor'];
- $nota = $_REQUEST['nota'];
+$nome = $_REQUEST['nome'];
+$professor = $_REQUEST['professor'];
+$nota = $_REQUEST['nota'];
  
  
- if(strlen($nome) == 0 || strlen($professor) == 0){
- $dados = array(
- 'tipo' => 'alert-warning',
- 'mensagem' => 'Por favor preencha todo o formulário!'
- );
- }else{
+if(strlen($nome) == 0 || strlen($professor) == 0){
+$dados = array(
+'tipo' => 'alert-warning',
+'mensagem' => 'Por favor preencha todo o formulário!'
+);
+}else{
 
- $sql = "INSERT INTO disciplinas (nome, professor, nota) VALUES ('".$nome."', '".$professor."', '".$nota."')";
+$sql = "INSERT INTO disciplina (nome, professor, nota) VALUES ('".$nome."', '".$professor."', '".$nota."')";
 
- if(mysqli_query($conecta, $sql)){
- $dados = array(
- 'tipo' => 'alert-success',
- 'mensagem' => 'A disciplina '.$nome.', foi salva com sucesso!'
- );
- }else{
- $dados = array(
- 'tipo' => 'alert-danger',
- 'mensagem' => 'Deu ruim....'.mysqli_error($conecta)
- );
- }
- }
+if(mysqli_query($conecta, $sql)){
+$dados = array(
+'tipo' => 'alert-success',
+'mensagem' => 'A disciplina '.$nome.', foi salva com sucesso!'
+);
+}else{
+$dados = array(
+'tipo' => 'alert-danger',
+'mensagem' => 'Deu ruim....'.mysqli_error($conecta)
+);
+}
+}
  
- echo json_encode($dados);
+echo json_encode($dados);
--- src/disciplina/modelo/listdisciplina.php
+++ src/disciplina/modelo/listdisciplina.php
@@ -1,16 +1,16 @@
 <?php
-// Instancio a minha conexão de banco de dados
+
 include('../../conexao/conn.php');
  
-// Criar uma query de consulta ao banco de dados
-$sql = "SELECT * FROM disciplinas";
+
+$sql = "SELECT * FROM disciplina";
  
-// Agora iremos executar nossa query SQL
+
 $resultado = mysqli_query($conecta, $sql);
  
-// Iremos testar nossa consulta ao banco de dados, para ver se ela nãoe stá retornando vazia
+
 if($resultado && mysqli_num_rows($resultado)>0){
-// Associar os registros encontrados em um array
+
 while($linha = mysqli_fetch_assoc($resultado)){
 $dados[] = array_map(null, $linha);
 }
