f2164d4b0f29c05afc58575066ee704304d4d92f
leticianb <leticianakahara10@gmail.com>
2020-08-25 10:28:02 -0300

    ..

--- src/conexao/disciplina/modelo/listdisciplina.php
+++ src/conexao/disciplina/modelo/listdisciplina.php
@@ -1,19 +1,21 @@
 <?php
-include('../../../conexao/conn.php');
-
-$sql = "SELECT * FROM disciplina";
-
-$resultado= mysqli_query($conecta, $sql);
+// Instancio a minha conexão de banco de dados
+include('../../conexao/conn.php');
+ 
+// Criar uma query de consulta ao banco de dados
+$sql = "SELECT * FROM disciplinas";
+ 
+// Agora iremos executar nossa query SQL
+$resultado = mysqli_query($conecta, $sql);
+ 
+// Iremos testar nossa consulta ao banco de dados, para ver se ela nãoe stá retornando vazia
 if($resultado && mysqli_num_rows($resultado)>0){
-
-    while($linha = mysqli_fetch_assoc($resultado)){
-        $dados[] = array_map('utf8_encode', $linha);
-
-
-    }
+// Associar os registros encontrados em um array
+while($linha = mysqli_fetch_assoc($resultado)){
+$dados[] = array_map(null, $linha);
 }
-    else{
-        $dados=array('erro'=> 'não foi possivel buscar resultados');
-    }
-
+}else{
+$dados = array('erro' => 'Não foi possível buscar resultados');
+}
+ 
 echo json_encode($dados);
