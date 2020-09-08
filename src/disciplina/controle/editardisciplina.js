570c37a78653730232ffdca587b06cc39f4b5c19
leticianb <leticianakahara10@gmail.com>
2020-09-08 19:19:52 -0300

    k

--- src/disciplina/controle/editardisciplina.js
+++ src/disciplina/controle/editardisciplina.js
@@ -26,13 +26,13 @@ $(document).ready(function() {
                     $('h4').empty()
                     $('h4').append('Edição de Registro')
                     $('.btn-save').after(' <button class="btn btn-secondary btn-block btn-close"><i class="mdi mdi-close"></i> Fechar</button>')
-                        //$('.btn-save').hide()
-                        //$('#nome').attr('disabled', true)
+
                     $('#nome').val(dados[0].nome)
-                        //$('#nota').attr('disabled', true)
-                    $('#nota').empty()
-                    $('#nota').append(`<option>${dados[0].nota}</option>`)
-                        //$('#professor').attr('disabled', true)
+
+
+
+                    $('#nota').append(`<option selected>${dados[0].nota}</option>`)
+
                     $('#professor').val(dados[0].professor)
 
                     btnclose()
