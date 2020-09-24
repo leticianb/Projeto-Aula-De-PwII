328b2d32e97e1f6908ba20c83c44da798b8e3e47
leticianb <leticianakahara10@gmail.com>
2020-09-23 21:59:56 -0300

    l

---
+++ src/alunos/controle/editaraluno.js
@@ -0,0 +1,44 @@
+function btnclose() {
+    $('.btn-close').click(function(e) {
+        e.preventDefault()
+        $('#form').empty()
+        $('#form').hide(2000)
+        $('.row').show(2000)
+    })
+}
+
+$(document).ready(function() {
+
+    $('.btn-view').click(function(e) {
+        e.preventDefault()
+        var dados = `id=${$(this).attr('id')}`
+        $.ajax({
+            type: 'POST',
+            dataType: 'JSON',
+            assync: true,
+            data: dados,
+            url: 'src/alunos/modelo/viewaluno.php',
+            success: function(dados) {
+                $('#form').show(2000)
+                $('.row').hide(2000)
+
+                $('#form').load('src/alunos/visao/adicionaaluno.html', function() {
+                    $('h4').empty()
+                    $('h4').append('Edição de Registro')
+                    $('.btn-save').after(' <button class="btn btn-secondary btn-block btn-close"><i class="mdi mdi-close"></i> Fechar</button>')
+                    $('.btn-save').addClass('btn-update').removeClass('btn-save')
+                    $('#nome').val(dados[0].nome)
+
+                    $('#tipo').append(`<option value="${dados[0].tipo}" selected>${dados[0].tipo == 1? 'administrador' : 'aluno'}</option>`)
+                    $('#curso').val(dados[0].curso)
+                    btnclose()
+                })
+                $('body').append('<script src="src/alunos/controle/updatealuno.js"></script>')
+
+            }
+        })
+
+
+    })
+
+})
