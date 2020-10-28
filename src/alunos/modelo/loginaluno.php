48ecd1083a34f3bd5db441c864c22df8ff2afe1e
leticianb <leticianakahara10@gmail.com>
2020-10-27 22:36:33 -0300

    a

--- src/alunos/modelo/loginaluno.php
+++ src/alunos/modelo/loginaluno.php
@@ -12,9 +12,18 @@ $sql = "SELECT * FROM alunos WHERE id = ".$id." AND senha='".$senha."'";
 
 $resultado = mysqli_query($conecta, $sql);
 if($resultado && mysqli_num_rows($resultado)>0){
+    while($lista)
+   while($dados = mysqli_fetch_array($resultado)){
+    session_start();
+    $_SESSION['id'] = $dados['id'];
+    $_SESSION['nome'] = $dados['nome'];
+    $_SESSION['curso'] = $dados['curso'];
+   }
+ 
     $dados = array('result'=> true);
 }
 else{
     $dados= array('result'=> false);
 }
+
 echo json_encode($dados);
