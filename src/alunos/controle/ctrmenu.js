8642f8512c4e351a82a102f8948b7b7079dd08b9
leticianb <leticianakahara10@gmail.com>
2020-10-27 23:32:58 -0300

    a

--- src/alunos/controle/ctrmenu.js
+++ src/alunos/controle/ctrmenu.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
 
+    $.ajax({
+        type: 'POST',
+        dataType: 'JSON',
+        assync: true,
+        data: dados,
+        url: 'src/alunos/modelo/tipoaluno.php',
+        success: function(dados) {
+
+        }
+    })
 })
---
+++ src/alunos/modelo/tipoaluno.php
@@ -0,0 +1,5 @@
+<?php
+session_start();
+
+$dados = array('tipo' => $_SESSION['tipo']);
+echo json_encode($dados);
