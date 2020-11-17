081975027846c222e046c2b74bfe4ef1a267c46c
leticianb <leticianakahara10@gmail.com>
2020-11-17 19:14:46 -0300

    a

--- src/disciplina/modelo/printboletim.php
+++ src/disciplina/modelo/printboletim.php
@@ -17,12 +17,41 @@ $resultado = mysqli_query($conecta, $sql);
  
 
 if($resultado && mysqli_num_rows($resultado)>0){
-    $imprimir='';
+    $imprimir=' 
+    <div class="container"> 
+    <div class="row">
+    <div class="col-12">
+    <div class="table-responsive">
+    <table class="table">
+        <thead class="table-danger">
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">DISCIPLINA</th>
+                <th scope="col">PROFESSOR</th>
+                <th scope="col">NOTA</th>
+               
+            </tr>
+        </thead>
+        <tbody>
+
+     ' ;
 
 while($print = mysqli_fetch_array($resultado)){
-    $imprimir= $imprimir.'<p>'.$print['nome'].'-'.$print['professor'].'-'.$print['nota'].'<p>';
-    
+   // $imprimir= $imprimir.'<p>'.$print['nome'].' - '.$print['professor'].' - '.$print['nota'].'<p>';
+    $imprimir = $imprimir.'<tr>
+    <td>'.$print.['id'].'</td>
+    <td>'.$print.['nome'].'</td>
+    <td>'.$print.['professor'].'</td>
+    <td>'.$print.['nota'].'</td>
+  
+</tr>';
 }
+$imprimir=$imprimir.'  </tbody>
+        </table>
+    </div>
+    </div>
+    </div>
+</div>';
 }else{
 $dados = array('erro' => 'Não foi possível buscar resultados');
 }
