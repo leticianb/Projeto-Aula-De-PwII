419ac875d0d706682960102e9c502c189cfb4805
leticianb <leticianakahara10@gmail.com>
2020-11-17 17:49:38 -0300

    a

--- src/disciplina/modelo/printboletim.php
+++ src/disciplina/modelo/printboletim.php
@@ -1,3 +1,22 @@
 <?php
 
 require_once '../../../new/dompdf/autoload.inc.php';
+
+use Dompdf\Dompdf;
+
+$dompdf = new Dompdf();
+
+$dompdf->loadHtml('
+<h1>Ola Mundo</h1>
+    <p>Meu PDF Meu PDF Meu PDF Meu PDF </p>
+    <p>Meu PDF Meu PDF Meu PDF Meu PDF</p>
+    <p>Meu PDF Meu PDF Meu PDF Meu PDF</p>
+');
+
+$dompdf->setPaper('A4', 'portrait');
+$dompdf->render();
+
+$dompdf->stream("boletim.pdf", array(true));
+
+
+
