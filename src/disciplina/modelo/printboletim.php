ac2073076b7d9dccc13faa13dc402e75e75b8bf2
leticianb <leticianakahara10@gmail.com>
2020-11-17 18:17:41 -0300

    a

--- src/disciplina/modelo/printboletim.php
+++ src/disciplina/modelo/printboletim.php
@@ -6,17 +6,40 @@ use Dompdf\Dompdf;
 
 $dompdf = new Dompdf();
 
-$dompdf->loadHtml('
-<h1>Ola Mundo</h1>
-    <p>Meu PDF Meu PDF Meu PDF Meu PDF </p>
-    <p>Meu PDF Meu PDF Meu PDF Meu PDF</p>
-    <p>Meu PDF Meu PDF Meu PDF Meu PDF</p>
-');
+include('../../conexao/conn.php');
+session_start();
+ 
 
-$dompdf->setPaper('A4', 'portrait');
-$dompdf->render();
+$sql = "SELECT * FROM disciplina WHERE id_alunos=".$_SESSION['id']."";
+ 
 
-$dompdf->stream("boletim.pdf", array(true));
+$resultado = mysqli_query($conecta, $sql);
+ 
+
+if($resultado && mysqli_num_rows($resultado)>0){
+
+while($print = mysqli_fetch_array($resultado)){
+    
+        echo '<p>'.$print['nome'].' - '.$print['professor'].' - '.$print['nota'].'<p>';
+    
+}
+}else{
+$dados = array('erro' => 'Não foi possível buscar resultados');
+}
+ 
+//echo json_encode($dados);
+
+//$dompdf->loadHtml('
+//<h1>Ola Mundo</h1>
+  //  <p>Meu PDF Meu PDF Meu PDF Meu PDF </p>
+    //<p>Meu PDF Meu PDF Meu PDF Meu PDF</p>
+    //<p>Meu PDF Meu PDF Meu PDF Meu PDF</p>
+//');
+
+//$dompdf->setPaper('A4', 'portrait');
+//$dompdf->render();
+
+//$dompdf->stream("boletim.pdf", array(true));
 
 
 
